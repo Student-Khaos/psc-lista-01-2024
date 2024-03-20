8ba7e7d7f1cf96e3a2f170436e6bd9dfeaba39d2
Khaos <155399918+Student-Khaos@users.noreply.github.com>
2024-03-19 21:25:09 -0300

    Java
    Uma maquina de vendas com várias opções. Apenas aceita notas de 2, 5, 10, 20, 50 e moedas de 1.

--- Maquina_de_vendas.java
+++ Maquina_de_vendas.java
@@ -7,9 +7,10 @@ public class Maquina_de_vendas {
    
 
     public static void main(String[] args) {
-      // Preços, variáveis
+      
         Scanner ler = new Scanner(System.in);
         
+        // Preços, variáveis
         int refrigerante = 3;
         int suco = 7;
         int vitamina = 9;
@@ -17,6 +18,7 @@ public class Maquina_de_vendas {
         int salgados = 3;
         int doces = 2;
      
+        //Imprimir
         System.out.println("Seja bem-vindo a Máquina de Vendas Automática");
         System.out.println("Escolha entre as seguintes opções: ");
         System.out.println("1 – Refrigerante R$3,00");
@@ -28,13 +30,15 @@ public class Maquina_de_vendas {
         System.out.print("Digite a opção desejada: ");
         int opcao = ler.nextInt();
         
+        //Nenhuma das Opções
         if (opcao < 1 || opcao > 8){
             System.out.println("Opção inválida");
         }
         
-        int valor = 0;
-        String produto = "";
+        int valor = 0;          //Valor do Produto
+        String produto = "";   //Produto
         
+        //Determinar valor e produto
         switch (opcao){
             case 1:
                 valor += refrigerante;
@@ -71,33 +75,36 @@ public class Maquina_de_vendas {
                 return;
         }
         
-       System.out.println("Insira o valor pago: ");
+        //Valor pago
+       System.out.println("Notas aceitas: 2, 5, 10, 20, 50 e moedas de 1 real. Insira o valor pago: ");
         int pago = ler.nextInt();
         
+        //Verificação do valor
         switch (pago) {
+            case 1:
             case 2:
             case 5:
             case 10:
             case 50:
                 break;
-                
+                 
+                //Nota invalida
             default:
-                System.out.println("Nota inválida. Insira uma nota de 2, 5, 10 ou 50.");
+                System.out.println("Nota inválida. Insira uma nota de 2, 5, 10, 50 ou moeda de 1.");
                 return;
         }
         
+        //Valor insuficiente
         if (pago < valor) {
             System.out.println("Valor pago insuficiente!");
         } 
                     
 
-
+        //Recibo
         else {
             
             int troco = pago - valor;
                         
-            
-
             System.out.println("Recibo");
             System.out.println("Produto: " + produto);
             System.out.println("Valor do produto: R$" + valor);
